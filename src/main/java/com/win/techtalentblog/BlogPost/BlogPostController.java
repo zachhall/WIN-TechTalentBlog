7a5583765edd91d6446dfdf59ffab555d83c9433
Zach Hall <zachhallnc@gmail.com>
2020-06-23 09:09:14 -0400

    Finish BlogPostController and add Result template

--- src/main/java/com/win/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/win/techtalentblog/BlogPost/BlogPostController.java
@@ -1,13 +1,30 @@
 package com.win.techtalentblog.BlogPost;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlogPostController {
 
+    @Autowired
+    private BlogPostRepository blogPostRepository;
+
     @GetMapping
     public String index(BlogPost blogPost) {
         return "blogpost/index";
+    }
+
+    private BlogPost blogPost;
+
+    @PostMapping
+    public String addNewBlogPost(BlogPost blogPost, Model model) {
+        blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+        model.addAttribute("title", blogPost.getTitle());
+        model.addAttribute("author", blogPost.getAuthor());
+        model.addAttribute("blogEntry", blogPost.getBlogEntry());
+        return "blogpost/result";
     }
 }
