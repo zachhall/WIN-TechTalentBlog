b79b831c4f4f54f2976b5868e7b0be48220f1278
Zach Hall <zachhallnc@gmail.com>
2020-06-23 08:30:20 -0400

    Add BlogPost class with basic constructor and G+S()

---
+++ src/main/java/com/win/techtalentblog/BlogPost/BlogPost.java
@@ -0,0 +1,42 @@
+package com.win.techtalentblog.BlogPost;
+
+public class BlogPost {
+    private Long id;
+    private String title, author, blogEntry;
+
+    public BlogPost(String title, String author, String blogEntry) {
+        this.title = title;
+        this.author = author;
+        this.blogEntry = blogEntry;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBlogEntry() {
+        return blogEntry;
+    }
+
+    public void setBlogEntry(String blogEntry) {
+        this.blogEntry = blogEntry;
+    }
+
+    @Override
+    public String toString() {
+        return "BlogPost [author=" + author + ", blogEntry=" + blogEntry + ", id=" + id + ", title=" + title + "]";
+    }
+
+}
